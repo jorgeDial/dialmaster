88605b607011920158fe5aee1f33f60141614690
frandial <francesco.lencioni@dialapplet.com>
2018-01-04 19:36:50 +0100

    Diseño grafico app

--- dialmaster.c
+++ dialmaster.c
@@ -1,104 +1,184 @@
+#include<stdlib.h>
 #include<ncurses.h>
 
 typedef struct 
 {
-    int counter;
+    WINDOW *w1;
+    WINDOW *w2;
+    WINDOW *w3;
+    int maxItemsProcess;
+    int maxItemsStates;
+    int maxItemsGeneralControls;
+    int maxItemsAdvancedControls;
+    int distanceW1;
+    int distanceW2;
+    int viewDetails;
+    char escapeApp; 
+    int com1;
+    int p;
+    int s;
+    int c;
     int activeWindow;
-}activateWindow;
+    float timeToRefresh;
+    int activeBrush;
+}Args;
 
-void refreshWindows(WINDOW *w1,WINDOW *w2)
+void clearWindows(Args Arg)
 {
-    //Bordes
-    box(w1,0,0); 
-    box(w2,0,0);
-    wrefresh(w1);
-    wrefresh(w2);
-    return;
+    int i;
+    for(i=0;i<10;i++) 
+    {
+        mvwprintw(Arg.w1,i,Arg.distanceW1,"%s","                               ");
+    }
 }
 
-void refreshWindow1(WINDOW *w1,char listProcess[][15],char listStates[][15],int p,int s,int distanceW1)
+void printProcess(Args Arg,char listProcess[][30],char listStates[][30],int i)
 {
+    //Función para dibujar la primera ventana que cambia frecuentemente
     //p -> variable de index process
     //s -> variable de index status
     char itemobjects[30];
-    sprintf(itemobjects,"%s - %s",listProcess[p],listStates[s]);
-    mvwprintw(w1,p+1,distanceW1,"%s",itemobjects);
+    sprintf(itemobjects,"%s  %s",listProcess[i],listStates[Arg.s]);
+    if(Arg.activeBrush)
+    {
+        wattron(Arg.w1,A_STANDOUT);
+    }
+    else
+    {
+        wattroff(Arg.w1,A_STANDOUT);
+    }
+    mvwprintw(Arg.w1,i+3,Arg.distanceW1,"%s",itemobjects);
+    return;
 }
 
-void refreshWindow2(WINDOW *w2,char listControls[][15],int c,int distanceW2)
+void printAdvancedControls(Args Arg,char listAdvancedControls[][30],int i)
 {
-    //c -> variable de index control
-    char itemobjects[30];
-    sprintf(itemobjects,"%s",listControls[c]);
-    mvwprintw(w2,c+1,distanceW2,"%s",itemobjects);
+    //Función para dibujar la primera ventana que cambia frecuentemente
+    //c -> variable de index controls
+    if(Arg.activeBrush)
+    {
+        wattron(Arg.w1,A_STANDOUT);
+    }
+    else
+    {
+        wattroff(Arg.w1,A_STANDOUT);
+    }
+    mvwprintw(Arg.w1,i+3,Arg.distanceW1,"%s",listAdvancedControls[i]);
+    return;
 }
 
-activateWindow activateWindows1(WINDOW *w1,char listProcess[][15],char listStates[][15],int p,int s,int com1,int maxItemsProcess,int distanceW1,int activeWindow)
+void refreshWindows(Args Arg,char listProcess[][30],char listStates[][30],char listAdvancedControls[][30],char listGeneralControls[][30])
 {
-    // margen derecho para la caja de elementos
-    refreshWindow1(w1,listProcess,listStates,p,s,distanceW1);
-    // si flecha arriba se sube en la listobjectsa, si flecha abajo se baja en la listobjectsa
-    switch(com1) 
+    //Función para dibujar las ventanas
+    clearWindows(Arg);
+    wattroff(Arg.w1,A_STANDOUT);
+    if(Arg.viewDetails==0)
+    {
+        mvwprintw(Arg.w1,1,Arg.distanceW1,"%s","PROCESOS             ESTADO");
+        wattron(Arg.w1,A_STANDOUT);
+        // imprimo listProcess - se seleciona el primer elemento
+        int i;
+        for(i=0;i<Arg.maxItemsProcess;i++) 
+        {
+            if(Arg.p<i) 
+            {
+                Arg.activeBrush=0;
+            }
+            else if(Arg.p==i)
+            {
+                Arg.activeBrush=1;
+            }
+            else
+            {
+                Arg.activeBrush=0;
+            }
+            printProcess(Arg,listProcess,listStates,i);
+        }
+    }
+    else
+    {
+        mvwprintw(Arg.w1,1,Arg.distanceW1,"%s%s%s","Proc: ",listProcess[Arg.p],listStates[Arg.s]);
+        wattron(Arg.w1,A_STANDOUT);
+        // imprimo listProcess - se seleciona el primer elemento
+        int i;
+        for(i=0;i<Arg.maxItemsAdvancedControls;i++) 
+        {
+            if(Arg.c<i) 
+            {
+                Arg.activeBrush=0;
+            }
+            else if(Arg.c==i)
+            {
+                Arg.activeBrush=1;
+            }
+            else
+            {
+                Arg.activeBrush=0;
+            }
+            printAdvancedControls(Arg,listAdvancedControls,i);
+        }
+    }
+    wattroff(Arg.w1,A_STANDOUT);
+    for(Arg.c=0;Arg.c<Arg.maxItemsGeneralControls;Arg.c++) 
+    {
+        mvwprintw(Arg.w2,2,(Arg.distanceW2*Arg.c)+2,"%s",listGeneralControls[Arg.c]);
+    }
+    box(Arg.w1,0,0); 
+    wrefresh(Arg.w1);
+    box(Arg.w2,0,0); 
+    wrefresh(Arg.w2);
+    box(Arg.w3,0,0); 
+    wrefresh(Arg.w3);
+    return;
+}
+
+Args activateWindows(Args Arg,char listProcess[][30],char listStates[][30],char listAdvancedControls[][30],char listGeneralControls[][30])
+{
+    //Función donde la primera ventana se activa
+    //Si flecha arriba se sube en la listobjectsa, si flecha abajo se baja en la listobjectsa
+    switch(Arg.com1) 
     {
         case KEY_UP:
         {
-            p--;
-            p = (p<0) ? maxItemsProcess-1 : p;
+            if(Arg.viewDetails==0)
+            {
+                Arg.p--;
+                Arg.p = (Arg.p<0) ? Arg.maxItemsProcess-1 : Arg.p;
+            }
+            else
+            {
+                Arg.c--;
+                Arg.c = (Arg.c<0) ? Arg.maxItemsAdvancedControls-1 : Arg.c;
+            }
             break;
         }
         case KEY_DOWN:
         {
-            p++;
-            p = (p>=maxItemsProcess) ? 0 : p;
+            if(Arg.viewDetails==0)
+            {
+                Arg.p++;
+                Arg.p = (Arg.p>=Arg.maxItemsProcess) ? 0 : Arg.p;
+            }
+            else
+            {
+                Arg.c++;
+                Arg.c = (Arg.c>=Arg.maxItemsAdvancedControls) ? 0 : Arg.c;
+            }
+            break;
+        }
+        case KEY_RIGHT:
+        {
+            Arg.viewDetails=1;
             break;
         }
         case KEY_LEFT:
         {
-            activeWindow=2;
-        }
-    }
-    // pinto el elemento seleccionado
-    wattron(w1,A_STANDOUT);
-    refreshWindow1(w1,listProcess,listStates,p,s,distanceW1);
-    wattroff(w1,A_STANDOUT);
-    activateWindow Res;
-    Res.counter=p;
-    Res.activeWindow=activeWindow;
-    return Res;
-}
-
-activateWindow activateWindows2(WINDOW *w2,char listControls[][15],int c,int com2,int maxItemsControls,int distanceW2,int activeWindow)
-{
-    // margen derecho para la caja de elementos
-    refreshWindow2(w2,listControls,c,distanceW2);
-    // si flecha arriba se sube en la listobjectsa, si flecha abajo se baja en la listobjectsa
-    switch(com2) 
-    {
-        case KEY_UP:
-        {
-            c--;
-            c = (c<0) ? maxItemsControls-1 : c;
+            Arg.viewDetails=0;
             break;
         }
-        case KEY_DOWN:
-        {
-            c++;
-            c = (c>=maxItemsControls) ? 0 : c;
-            break;
-        }
-        case KEY_LEFT:
-        {
-            activeWindow=1;
-        }
     }
-    // pinto el elemento seleccionado
-    wattron(w2,A_STANDOUT);
-    refreshWindow2(w2,listControls,c,distanceW2);
-    wattroff(w2,A_STANDOUT);
-    activateWindow Res;
-    Res.counter=c;
-    Res.activeWindow=activeWindow;
-    return Res;
+    refreshWindows(Arg,listProcess,listStates,listAdvancedControls,listGeneralControls);
+    return Arg;
 }
 
 void main() 
@@ -106,87 +186,78 @@ void main()
     // se inicializa Ncurses
     initscr();
     crmode();
-    noecho();
     /* Inicializaciones variables */
-    WINDOW *w1;
-    WINDOW *w2;
-    w1 = newwin(10,30,1,1); // nueva ventana (Lineas, Columnas, posición Y, posición X)
-    w2 = newwin(15,25,1,30); // nueva ventana (Lineas, Columnas, posición Y, posición X)
-    int maxItemsProcess=5;
-    int maxItemsControls=4;
-    char listProcess[][15] = { "Dialserver", "Dialreport", "Dialcontact", "Asterisk", "ipTables" };
-    char listStates[][15] = { "OK", "Died", "Starting...", "Stopping...", "???"};
-    char listControls[][15] = { "Start", "Stop", "Restart", "Reload"};
-    int distanceW1=2;
-    int distanceW2=10;
-    char escapeApp='q'; 
-    int com1,com2,p=0,s=2,c=1,activeWindow=1;
-    activateWindow Res;
+    Args Arg;
+    // nuevas ventanas (Lineas, Columnas, posición Y, posición X)
+    Arg.w1 = newwin(26,35,1,1); 
+    Arg.w2 = newwin(5,155,27,1);
+    Arg.w3 = newwin(26,120,1,36);
+    char listProcess[5][30]={"DialServer ","DialReport ","DialContact","Asterisk   ","ipTables   "};
+    char listStates[5][30]={"            OK","           RIP","   Starting...","   Stopping...","           ???"};
+    char listGeneralControls[3][30]={"F1-MainLog  ","F2-WebServiceLog   ","F3-SpreadLog"};
+    char listAdvancedControls[4][30]={"start","stop","reload","restart"};
+    Arg.maxItemsProcess=sizeof(listProcess)/sizeof(listProcess[0]);
+    Arg.maxItemsStates=sizeof(listStates)/sizeof(listStates[0]);
+    Arg.maxItemsGeneralControls=sizeof(listGeneralControls)/sizeof(listGeneralControls[0]);
+    Arg.maxItemsAdvancedControls=sizeof(listAdvancedControls)/sizeof(listAdvancedControls[0]);
+    Arg.distanceW1=2;
+    Arg.distanceW2=30;
+    Arg.viewDetails=0;
+    Arg.escapeApp='q'; 
+    Arg.com1;
+    Arg.p=0;
+    Arg.s=0;
+    Arg.c=0;
+    Arg.activeWindow=1;
+    Arg.timeToRefresh=1;
+    Arg.activeBrush=1;
 
     /* Init App */
-    refreshWindows(w1,w2);
-    
+    mvwprintw(Arg.w1,1,Arg.distanceW1,"%s","PROCESOS             ESTADO");
     // imprimo listProcess - se seleciona el primer elemento
-    for(p=0;p<maxItemsProcess;p++) 
+    for(Arg.p=0;Arg.p<Arg.maxItemsProcess;Arg.p++) 
     {
-        if(p==0) 
+        if(Arg.p==0) 
         {
-            s=3;
-            wattron(w1,A_STANDOUT); // se enciende el primer itemobjects
+            // se enciende el primer itemobjects
+            Arg.activeBrush=1;
         }
         else
         {
-            s=1;
-            wattroff(w1,A_STANDOUT); // se apaga el primer itemobjects
+            // se apaga el primer itemobjects
+            Arg.activeBrush=0;
         }
-        refreshWindow1(w1,listProcess,listStates,p,s,distanceW1);
-    }
-    
-    for(c=0;c<maxItemsControls;c++) 
-    {
-        if(c==0) 
-        {
-            wattron(w2,A_STANDOUT); // se enciende el primer itemobjects
-        }
-        else
-        {
-            wattroff(w2,A_STANDOUT); // se apaga el primer itemobjects
-        }
-        refreshWindow2(w2,listControls,c,distanceW2);
+        printProcess(Arg,listProcess,listStates,Arg.p);
     }
 
+    for(Arg.c=0;Arg.c<Arg.maxItemsGeneralControls;Arg.c++) 
+    {
+        mvwprintw(Arg.w2,2,(Arg.distanceW2*Arg.c)+2,"%s",listGeneralControls[Arg.c]);
+    }
     // se refresca las consolas
-    wrefresh(w1); 
-    wrefresh(w2); 
+    box(Arg.w1,0,0); 
+    wrefresh(Arg.w1);
+    box(Arg.w2,0,0); 
+    wrefresh(Arg.w2);
+    box(Arg.w3,0,0); 
+    wrefresh(Arg.w3);
  
-    p=0;
-    c=0;
+    Arg.p=0;
+    Arg.c=0;
     noecho();
 
-    keypad(w1,TRUE); // se permite meter numeros
-    keypad(w2,TRUE); // se permite meter numeros
+    keypad(Arg.w1,TRUE); // se permite meter numeros
     curs_set(0); // se esconde el cursor normal
 
     //entrada teclado
-    while((com1=wgetch(w1))!=escapeApp)
-    { 
-        com2=wgetch(w2);
-        if(activeWindow==1)
+    while((Arg.com1=wgetch(Arg.w1))!=Arg.escapeApp)
+    {
+        if(Arg.activeWindow==1)
         {
-            Res=activateWindows1(w1,listProcess,listStates,p,s,com1,maxItemsProcess,distanceW1,activeWindow);
-            p=Res.counter;
-            activeWindow=Res.activeWindow;
-            //printf("\nMess:1 - Counter: %d - ActiveWindow: %d",p,activeWindow);
-        }
-        else if(activeWindow==2)
-        {
-            Res=activateWindows2(w2,listControls,c,com2,maxItemsControls,distanceW2,activeWindow);
-            c=Res.counter;
-            activeWindow=Res.activeWindow;
-            //printf("\nMess:2 - Counter: %d - ActiveWindow: %d",c,activeWindow);
+            Arg=activateWindows(Arg,listProcess,listStates,listAdvancedControls,listGeneralControls);
         }
     }
-    delwin( w1 );
-    delwin( w2 );
+    //Cierro las ventanas
+    delwin(Arg.w1);
     endwin();
 }
